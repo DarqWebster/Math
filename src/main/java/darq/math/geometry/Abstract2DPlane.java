7469350d6ee570cd355351aa54ebbaba455ccecb
Darq <darq.webster@gmail.com>
2016-04-11 21:44:48 +0200

    Added method documentation. Deleted some commented-out code.

--- src/main/java/darq/math/geometry/Abstract2DPlane.java
+++ src/main/java/darq/math/geometry/Abstract2DPlane.java
@@ -18,12 +18,44 @@ import java.util.stream.Collector;
  * @author Craig.Webster
  */
 public abstract class Abstract2DPlane {
+	/**
+	 * Returns the length of the line drawn from the origin, to the given point.
+	 * The line is drawn from (0, 0) to (yD, xD).
+	 * To be completed by the subclass.
+	 * @param yD
+	 * @param xD
+	 * @return The length of the line drawn from (0, 0) to (yD, xD).
+	 */
 	public abstract double distance(double yD, double xD);
 	
+	/**
+	 * Calculate the distance between the given points.
+	 * Uses the subclass's implementation of <code>distance(double, double)</code>.
+	 * @param p1
+	 * @param p2
+	 * @return 
+	 */
 	public double distance(Point p1, Point p2) {
 		return distance(p2.y - p1.y, p2.x - p1.x);
 	}
 	
+	/**
+	 * Calculate the angle from "North" to the line defined by the given deltas.
+	 * The returned value ranges from 0 to 2 * PI radians.
+	 * 
+	 * For example, the angle A below is greater than PI / 2 and less than PI.
+	 *         |
+	 *         |__
+	 *         |   \
+	 * ----------A------
+	 *         |\_ /
+	 *         | \
+	 *         |  \
+	 * 
+	 * @param yD
+	 * @param xD
+	 * @return The angle, in radians, between "north" and the given line.
+	 */
 	public double angle(double yD, double xD) {
 		if (Utils.equals(yD, 0) && Utils.equals(xD, 0)) {
 			return Double.NaN;
@@ -39,10 +71,24 @@ public abstract class Abstract2DPlane {
 		return angle(to.y - from.y, to.x - from.x);
 	}
 	
+	/**
+	 * Move the given Point by the given deltas.
+	 * @param point
+	 * @param yD
+	 * @param xD
+	 * @return The given Point moved yD on the y-axis and xD on the x-axis.
+	 */
 	public Point adjust(Point point, double yD, double xD) {
 		return new Point(point.y + yD, point.x + xD);
 	}
 	
+	/**
+	 * Move the given Segment by the given deltas.
+	 * @param segment
+	 * @param yD
+	 * @param xD
+	 * @return The given Segment moved yD on the y-axis and xD on the x-axis.
+	 */
 	public Segment adjust(Segment segment, double yD, double xD) {
 		return new Segment(adjust(segment.pS, yD, xD), adjust(segment.pE, yD, xD));
 	}
@@ -60,6 +106,14 @@ public abstract class Abstract2DPlane {
 		)));
 	}
 	
+	/**
+	 * Calculate the Point,
+	 * <code>step</code> distance along the line defined by the given deltas.
+	 * @param yD
+	 * @param xD
+	 * @param step
+	 * @return The Point along the line defined by the given Points, at the distance defined by step.
+	 */
 	public Point getDeltaInDirection(double yD, double xD, double step) {
 		double reference = distance(yD, xD);
 		double ratio = step / reference;
@@ -67,16 +121,75 @@ public abstract class Abstract2DPlane {
 	}
 	
 	/**
-	 *
+	 * Calculates where the given Point is, relative to the given Segment.
+	 * The Segment defined a line infinitely long in both directions.
+	 * Terms "left" and "right" are defined relative to the Segment,
+	 * standing at the start Point of the Segment,
+	 * looking towards the end Point of the Segment.
+	 * 
+	 * For example:
+	 *         E
+	 *        /
+	 *     L / R
+	 *      /
+	 *     S
+	 * 
+	 * And:
+	 *     S
+	 *      \
+	 *     R \ L
+	 *        \
+	 *         E
+	 * 
 	 * @param p
 	 * @param s
-	 * @return -1 if p is to the left of s, 0 if p and s are colinear, and 1 if  p is to the right of s.
+	 * @return -1 if p is to the left of s,
+	 * 0 if p and s are colinear,
+	 * and 1 if  p is to the right of s.
 	 */
 	public int compare(Point p, Segment s) {
 		return Utils.sign((s.pE.y - s.pS.y) * (p.x - s.pS.x) - (s.pE.x - s.pS.x) * (p.y - s.pS.y));
 	}
 	
+	/**
+	 * Calculates where the given segments intersect.
+	 * 
+	 * The intersection is returned as a value <code>u</code> for each Segment.
+	 * <code>u</code> is 0 at the start Point of the Segment,
+	 * <code>u</code> is 1 at the end Point of the Segment.
+	 * 
+	 * Each Segment represents an infinite line,
+	 * intersections may occur before the start Point or after the end Point.
+	 * <code>u</code> is less than 0 before the start Point,
+	 * <code>u</code> is greater than 1 after the end Point.
+	 * 
+	 * Returns an array containing both value of <code>u</code>,
+	 * index 0 contains the <code>u</code> of the first Segment,
+	 * index 1 contains the <code>u</code> of the second Segment.
+	 * Returns <code>null</code> if the given Segments are parallel,
+	 * and therefore do not intersect.
+	 * 
+	 * For example:
+	 * Given a Segment defined by start point S and end point E, with length D,
+	 * the value of <code>u</code> is plotted at various points on the line:
+	 * 
+	 * -1       -0.5        0        0.5        1        1.5        2
+	 *  |         |         |   0.25  |   0.75  |         |         |
+	 *  |         |         |    |    |    |    |         |         |
+	 *  --------------------S-------------------E--------------------
+	 *  |_________D_________|_________D_________|_________D_________|
+	 * 
+	 * Returns <code>null</code> if the given Segments are parallel.
+	 * 
+	 * @param s1
+	 * @param s2
+	 * @return An array of two (2) double values,
+	 * index 0 contains the <code>u</code> of the first Segment,
+	 * index 1 contains the <code>u</code> of the second Segment,
+	 * or <code>null</code> if the given Segments are parallel.
+	 */
 	public double[] collidesAt(Segment s1, Segment s2) {
+		// Explanation of the math below:
 		// Any point on s1 can be defined as "s1.pS + u1 * (s1.pE − s1.pS)".
 		// Where u1 is a percentage of the distance between s1.pS and s1.pE.
 		// This simplifies down to the two equations:
@@ -107,6 +220,19 @@ public abstract class Abstract2DPlane {
 		return new double[] {u1, u2};
 	}
 	
+	/**
+	 * Calculates a Point along the given Segment, based on the given double.
+	 * Converts the <code>u</code> value,
+	 * calculated in <code>collidesAt(Segment, Segment)</code>,
+	 * into a usable Point.
+	 * 
+	 * <code>u</code> is defined in the documentation for
+	 * <code>collidesAt(Segment, Segment)</code>.
+	 * 
+	 * @param segment
+	 * @param u
+	 * @return 
+	 */
 	public Point getPointAlongSegment(Segment segment, double u) {
 		double y = segment.pS.y + u * (segment.pE.y - segment.pS.y);
 		double x = segment.pS.x + u * (segment.pE.x - segment.pS.x);
@@ -115,32 +241,45 @@ public abstract class Abstract2DPlane {
 	}
 	
 	/**
+	 * Calculates if the given Segment contains the given Point.
+	 * A Segment "contains" a Point when the line drawn from
+	 * the start Point to the end Point crosses the given Point.
 	 * 
 	 * @param s
 	 * @param p
-	 * @return true if p is on s, false otherwise.
+	 * @return true if Point p is on Segment s, false otherwise.
 	 */
 	public boolean contains(Segment s, Point p) {
-//		double yD = segment.pE.y - segment.pS.y;
-//		double xD = segment.pE.x - segment.pS.x;
-//		
-//		Segment perpendicular = new Segment(point, new Point(point.y - xD, point.x + yD));
-//		double[] u = collidesAt(segment, perpendicular);
-//		
-//		return Utils.gte(u[0], 0) && Utils.lte(u[0], 1) && Utils.equals(u[1], 0);
-		
-//		return collidesAt(new Segment(segment.pS, point), new Segment(point, segment.pE)) == null &&
-//				Utils.equals(distance(segment.pS, point) + distance(point, segment.pE), distance(segment.pS, segment.pE));
-		
 		return Utils.equals((p.y - s.pS.y) / (s.pE.y - s.pS.y), (p.x - s.pS.x) / (s.pE.x - s.pS.x)) &&
 				Utils.sign(s.pS.y - p.y) * Utils.sign(s.pS.y - s.pE.y) >= 0 && Utils.sign(s.pE.y - p.y) * Utils.sign(s.pE.y - s.pS.y) >= 0;
 	}
 	
 	/**
+	 * Calculates if the given Polygon contains the given Point.
+	 * Returns an <code>int</code> value indicating the position of the Point,
+	 * relative to the given Polygon.
+	 * The value is -1 if the Point is external to the Polygon,
+	 * 0 if the Point occurs on a Segment of the Polygon,
+	 * 1 if the Point is internal and completely contained within the Polygon.
+	 * 
+	 * For example:
+	 *      ____
+	 *     /    \  .A
+	 *    /  .C  \
+	 *    \      /
+	 *     \__._/
+	 *         B
+	 * 
+	 * Given the above Polygon,
+	 * Point A would return -1,
+	 * Point B would return 0,
+	 * Point C would return 1.
 	 * 
 	 * @param polygon
 	 * @param point
-	 * @return -1 if point is external to polygon, 0 if point is on one of segments in polygon, and 1 of point is internal to polygon.
+	 * @return -1 if point is external to polygon,
+	 * 0 if point is on one of segments in polygon,
+	 * and 1 of point is internal to polygon.
 	 */
 	public int contains(Polygon polygon, Point point) {
 		Collection<Segment> segments = new ArrayList<Segment>(polygon.segments.size());
@@ -188,34 +327,95 @@ public abstract class Abstract2DPlane {
 		}
 	}
 	
+	/**
+	 * Calculates if the given Segments overlap.
+	 * To overlap, Segments must be completely collinear,
+	 * and must each contain a common Point.
+	 * 
+	 * @param s1
+	 * @param s2
+	 * @return true if the given Segments overlap, false otherwise.
+	 */
 	public boolean overlaps(Segment s1, Segment s2) {
 		return collidesAt(s1, s2) == null && (Abstract2DPlane.this.contains(s1, s2.pS) || Abstract2DPlane.this.contains(s1, s2.pS) || Abstract2DPlane.this.contains(s2, s1.pS) || Abstract2DPlane.this.contains(s2, s1.pE));
 	}
 	
 	/**
 	 * Returns the left normal of the given segment.
-	 * The returned segment is perpendicular to the given segment,
+	 * The returned Segment is perpendicular to the given Segment,
 	 * shares a start point with the given segment,
 	 * and has an end point to the left of the given segment.
+	 * 
+	 * For example:
+	 *          E -|
+	 *          |  |
+	 *    Pi/2__|  | Given segment
+	 *       |  |  |
+	 * E--------S -|
+	 * |________|
+	 *   Normal
+	 * 
 	 * @param s
-	 * @return 
+	 * @return The normal to the left of the give Segment
 	 */
 	public Segment normalL(Segment s) {
 		return new Segment(s.pS, new Point(s.pS.y + (s.pE.x - s.pS.x), s.pS.x + (s.pS.y - s.pE.y)));
 	}
 	
-		/**
+	/**
 	 * Returns the right normal of the given segment.
 	 * The returned segment is perpendicular to the given segment,
 	 * shares a start point with the given segment,
 	 * and has an end point to the right of the given segment.
+	 * 
+	 * For example:
+	 *               E
+	 *               |
+	 * Given segment |__Pi/2
+	 *               |  |
+	 *               S--------E -|
+	 *               |________|
+	 *                 Normal
+	 * 
 	 * @param s
-	 * @return 
+	 * @return The normal to the right of the given Segment.
 	 */
 	public Segment normalR(Segment s) {
 		return new Segment(s.pS, new Point(s.pS.y + (s.pS.x - s.pE.x), s.pS.x +(s.pE.y - s.pS.y)));
 	}
 	
+	/**
+	 * Returns the FOV from the given Point, blocked by the given Segments.
+	 * The field-of-view (FOV) is defined as a collection of Triangles,
+	 * all with the given Point as their first vertex,
+	 * and extending to collide with one of the given segments.
+	 * 
+	 * Any Point contained by the returned Triangles is within the FOV,
+	 * it is possible to draw a line between it and the given Point,
+	 * that does not intersect any of the given Segments.
+	 * 
+	 * Additionally, for each Triangle,
+	 * the Segment with which it collided is returned.
+	 * The second Segment of the Triangle will be contained by this Segment.
+	 * 
+	 * As an implementation tip:
+	 * Assuming the given Point is fully enclosed in the given Segments,
+	 * every Triangle will be adjacent to two other Triangles.
+	 * A point that is on a vertex, is on the edge of the FOV.
+	 * A point that is on exactly one Segment of the Triangles,
+	 * is on the edge of the FOV.
+	 * A point that is on exactly two Segments, and not on a vertex,
+	 * is contained within the FOV.
+	 * 
+	 * It is better to call <code>getFOV(Point, Collection, double)</code>,
+	 * to guarantee that the given Point is fully enclosed.
+	 * 
+	 * @param centre
+	 * @param segments
+	 * @return A Map of Triangle to Segments,
+	 * the Triangles defining the field-of-view,
+	 * along with the Segment with which the Triangle collided.
+	 */
 	public Map<Triangle, Segment> getFOV(Point centre, Collection<Segment> segments) {
 		Map<Triangle, Segment> triangles = new LinkedHashMap<Triangle, Segment>();
 		if (segments.isEmpty()) {
@@ -298,6 +498,21 @@ public abstract class Abstract2DPlane {
 		return triangles;
 	}
 	
+	/**
+	 * Completely encloses the given Point, before calculating FOV.
+	 * Encloses the given Point with Segments, at <code>limit</code> distance,
+	 * one segment along the x-axis at <code>centre.y - limit</code>,
+	 * one segment along the x-axis at <code>centre.y + limit</code>,
+	 * one segment along the y-axis at <code>centre.x - limit</code>,
+	 * one segment along the y-axis at <code>centre.x + limit</code>.
+	 * 
+	 * This functions identically to <code>getFOV(Point, Collection)</code>.
+	 * 
+	 * @param centre
+	 * @param segments
+	 * @param limit
+	 * @return 
+	 */
 	public Map<Triangle, Segment> getFOV(Point centre, Collection<Segment> segments, double limit) {
 		Collection<Segment> modSegments = new ArrayList<Segment>(segments.size() + 4);
 		
@@ -311,12 +526,15 @@ public abstract class Abstract2DPlane {
 	}
 	
 	/**
-	 * Sorts segments clockwise based on start point,
-	 * ensures end points are always clockwise to start points,
-	 * and removes colinear segments.
+	 * Sorts Segments clockwise based on the Segment start point,
+	 * ensures end Points are always clockwise from start Points,
+	 * and removes collinear Segments.
+	 * 
 	 * @param segments
 	 * @param relativeTo
-	 * @return 
+	 * @return The given Segments, sorted clockwise,
+	 * with end Points always clockwise from start Points,
+	 * with collinear Segments removed.
 	 */
 	private Collection<Segment> normaliseSegments(Collection<Segment> segments, Point relativeTo) {
 		TreeSet<Segment> sorted = new TreeSet<Segment>((segment1, segment2) -> {
@@ -361,14 +579,17 @@ public abstract class Abstract2DPlane {
 	}
 	
 	/**
-	 * Returns the closest collisions along the given ray.
-	 * Returns an array of two collision elements,
-	 * collision[0] is on a segment that extends left of the collision point,
-	 * collision[1] is on a segment that extends right of the collision point.
+	 * Returns the closest Collisions along the given ray.
+	 * Returns an array of two Collision elements,
+	 * collision[0] is on a Segment that extends left of the Collision Point,
+	 * collision[1] is on a Segment that extends right of the Collision Point.
 	 * Assumes that the Collection of segments has been normalised.
+	 * 
 	 * @param ray
 	 * @param normSegs
-	 * @return 
+	 * @return The closest Collisions along the given ray,
+	 * index 0 extends left of the Collision Point,
+	 * index 1 extends right of the Collision Point.
 	 */
 	private Collision[] getNextCollisions(Segment ray, Collection<Segment> normSegs) {
 		List<Collision> antiClockwise = new ArrayList<Collision>();
@@ -420,14 +641,15 @@ public abstract class Abstract2DPlane {
 	}
 	
 	/**
-	 * Returns the closest collision along the given ray.
+	 * Returns the closest Collision along the given ray.
 	 * A slightly faster implementation of getNextCollisions,
-	 * which only returns the next clockwise collision.
+	 * which only returns the next clockwise Collision.
 	 * Intended to be used when processing points in a clockwise manner.
-	 * Assumes that the Collection of segments has been normalised.
+	 * Assumes that the Collection of Segments has been normalised.
+	 * 
 	 * @param ray
 	 * @param normSegs
-	 * @return 
+	 * @return The closest clockwise Collision along the given ray.
 	 */
 	private Collision getNextCollision(Segment ray, Collection<Segment> normSegs) {		
 		List<Collision> collisions = new LinkedList<Collision>();
@@ -461,6 +683,9 @@ public abstract class Abstract2DPlane {
 		return closestCollision;
 	}
 	
+	/**
+	 * Represents a collision between a ray and a line segment.
+	 */
 	public class Collision {
 		public final Point point;
 		public final Segment segment;
@@ -472,14 +697,21 @@ public abstract class Abstract2DPlane {
 	}
 	
 	/**
-	 * Determines if the given polygons are colliding.
-	 * Returns null if the given polygons are not colliding,
-	 * otherwise, returns the smallest possible vector,
+	 * Determines if the given Polygons are colliding.
+	 * Returns null if the given Polygons are not colliding,
+	 * otherwise, returns a Segment representing the smallest possible vector,
 	 * that if applied to the second argument,
-	 * will cause the polygons to no longer collide.
+	 * will cause the Polygons to no longer collide.
+	 * 
+	 * This is an implementation of the Separating Axis Theorem.
+	 * 
 	 * @param p1
 	 * @param p2
-	 * @return 
+	 * @return The a Segment representing smallest possible vector,
+	 * that if applied to the second argument,
+	 * will cause the Polygons to no longer collide,
+	 * or null if the Polygons are not colliding.
+	 * @throws IllegalArgumentException If either given Polygon is not convex.
 	 */
 	public Segment collides(Polygon p1, Polygon p2) {
 		if (!isConvex(p1)) {
@@ -553,6 +785,13 @@ public abstract class Abstract2DPlane {
 		return new Segment(new Point(0, 0), getDeltaInDirection(distNorm.pE.y - distNorm.pS.y, distNorm.pE.x - distNorm.pS.x, dist * distMult));
 	}
 	
+	/**
+	 * Determines if a given Polygon is convex.
+	 * A polygon is convex if all interior angles are less than Pi radians.
+	 * 
+	 * @param polygon
+	 * @return true if the given Polygon is convex, false otherwise.
+	 */
 	private boolean isConvex(Polygon polygon) {
 		boolean clockwise = true;
 		
